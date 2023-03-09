50a6f879948c7ab70356cf5d495979556dc1e73d
Adam Ruminski <cssrumi@gmail.com>
2023-03-09 23:33:03 +0100

    event update

--- src/main/kotlin/pl/airq/ga/radon/domain/RadonMeasurementConsumer.kt
+++ src/main/kotlin/pl/airq/ga/radon/domain/RadonMeasurementConsumer.kt
@@ -1,7 +1,7 @@
 package pl.airq.ga.radon.domain
 
+import io.smallrye.reactive.messaging.annotations.Blocking
 import org.eclipse.microprofile.reactive.messaging.Incoming
-import org.eclipse.microprofile.reactive.messaging.Message
 import org.eclipse.microprofile.reactive.messaging.OnOverflow
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -9,7 +9,6 @@ import pl.airq.ga.radon.config.Topics
 import pl.airq.ga.radon.domain.event.RadonMeasurementEvent
 import pl.airq.ga.radon.domain.event.RadonMeasurementPayload
 import pl.airq.ga.radon.domain.port.measurement.MeasurementListener
-import java.util.concurrent.CompletionStage
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.inject.Instance
 
@@ -21,10 +20,10 @@ class RadonMeasurementConsumer(
 
     @OnOverflow(value = OnOverflow.Strategy.BUFFER, bufferSize = Topics.RADON_MEASUREMENTS_BUFFER_SIZE)
     @Incoming(Topics.RADON_MEASUREMENTS)
-    fun consume(message: Message<RadonMeasurementEvent>): CompletionStage<Void> {
-        message.payload.payload?.let { processPayload(it) }
+    @Blocking
+    fun consume(event: RadonMeasurementEvent) {
+        event.payload?.let { processPayload(it) }
             ?: LOGGER.info("Unhandled measurement event...")
-        return message.ack()
     }
 
     private fun processPayload(payload: RadonMeasurementPayload) {

b5686c07238ffbaad042c2b004c295d9489a26da
Adam Ruminski <cssrumi@gmail.com>
2023-03-09 23:24:28 +0100

    event update

--- src/main/kotlin/pl/airq/ga/radon/adapters/prediction/PredictionMeasurementListener.kt
+++ src/main/kotlin/pl/airq/ga/radon/adapters/prediction/PredictionMeasurementListener.kt
@@ -1,17 +1,18 @@
 package pl.airq.ga.radon.adapters.prediction
 
-import pl.airq.ga.radon.domain.PredictionService
 import pl.airq.ga.radon.domain.model.Measurement
+import pl.airq.ga.radon.domain.model.SensorId
+import pl.airq.ga.radon.domain.port.UniqueQueue
 import pl.airq.ga.radon.domain.port.measurement.MeasurementListener
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class PredictionMeasurementListener(
-    private val predictionService: PredictionService
+    private val predictionQueue: UniqueQueue<SensorId>
 ) : MeasurementListener {
 
     override fun listen(measurement: Measurement) {
-        predictionService.predict(measurement.sensorId)
+        predictionQueue.put(measurement.sensorId)
     }
 
     override fun order() = 0
